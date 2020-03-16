bad1c5b15b0033e768d04148c8e82c91c1698fbe
RV <rgvegajr@gmail.com>
2020-03-15 23:07:24 -0400

    added additional content and urls for original app templates

---
+++ client/src/views/Body/SignupPage.js
@@ -0,0 +1,166 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Form,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Container,
+  Col,
+  NavLink
+} from "reactstrap";
+
+// core components
+import HomeNavbar from "components/Navbars/HomeNavbar.js";
+import TransparentFooter from "components/Footers/TransparentFooter.js";
+
+function SignupPage() {
+  const [firstFocus, setFirstFocus] = React.useState(false);
+  const [lastFocus, setLastFocus] = React.useState(false);
+  React.useEffect(() => {
+    document.body.classList.add("signup-page");
+    document.body.classList.add("sidebar-collapse");
+    document.documentElement.classList.remove("nav-open");
+    window.scrollTo(0, 0);
+    document.body.scrollTop = 0;
+    return function cleanup() {
+      document.body.classList.remove("signup-page");
+      document.body.classList.remove("sidebar-collapse");
+    };
+  });
+  return (
+    <>
+      <HomeNavbar />
+      <div className="homepage-header clear-filter" filter-color="blue">
+        <div
+          className="homepage-header-image"
+          style={{
+            backgroundImage: "url(" + require("assets/img/newVice.jpg") + ")"
+          }}
+        ></div>
+        <div className="content">
+          <Container>
+            <Col className="ml-auto mr-auto" md="4">
+              <Card className="card-login card-plain">
+                <Form action="" className="form" method="">
+                  <CardHeader className="text-center">               
+                  </CardHeader>
+                  <CardBody>
+                    <InputGroup
+                      className={
+                        "no-border input-lg" +
+                        (firstFocus ? " input-group-focus" : "")
+                      }
+                    >
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="now-ui-icons users_circle-08"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input
+                        value="username"
+                        name="username"
+                        placeholder="User Name..."
+                        type="text"
+                        onFocus={() => setFirstFocus(true)}
+                        onBlur={() => setFirstFocus(false)}
+                      ></Input>
+                    </InputGroup>
+                    <InputGroup
+                      className={
+                        "no-border input-lg" +
+                        (firstFocus ? " input-group-focus" : "")
+                      }
+                    >
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="now-ui-icons ui-1_email-85"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input
+                        value="email"
+                        name="email"
+                        placeholder="Email..."
+                        type="email"
+                        onFocus={() => setFirstFocus(true)}
+                        onBlur={() => setFirstFocus(false)}
+                      ></Input>
+                    </InputGroup>
+                    <InputGroup
+                      className={
+                        "no-border input-lg" +
+                        (lastFocus ? " input-group-focus" : "")
+                      }
+                    >
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="now-ui-icons objects_key-25"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input
+                        value="password"
+                        name="password"
+                        placeholder="Password..."
+                        type="text"
+                        onFocus={() => setLastFocus(true)}
+                        onBlur={() => setLastFocus(false)}
+                      ></Input>
+                    </InputGroup>
+                  </CardBody>
+                  <CardFooter className="text-center">
+                    <Button
+                      block
+                      className="btn-round"
+                      color="info"
+                      href="#pablo"
+                      onClick={e => e.preventDefault()}
+                      size="lg"
+                    >
+                      Create Account!
+                    </Button>
+                    <div className="pull-left">
+                      <h6>
+                      <NavLink to="/login-page" tag={Link}>
+                <i className="now-ui-icons users_circle-08"></i>Login!
+                </NavLink>
+                        <a
+                          className="link"
+                          href="#pablo"
+                          onClick={e => e.preventDefault()}
+                        >
+                          Create Account
+                        </a>
+                      </h6>
+                    </div>
+                    <div className="pull-right">
+                      <h6>
+                        <a
+                          className="link"
+                          href="#pablo"
+                          onClick={e => e.preventDefault()}
+                        >
+                          Need Help?
+                        </a>
+                      </h6>
+                    </div>
+                  </CardFooter>
+                </Form>
+              </Card>
+            </Col>
+          </Container>
+        </div>
+        <TransparentFooter />
+      </div>
+    </>
+  );
+}
+
+export default SignupPage;
